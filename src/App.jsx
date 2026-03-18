7137f6e9e736501bd58e10fc1855b6d8950c7196
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-03-18 10:07:50 +0100

    Added link to GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Mona Lang and is open-sourced on GitHub.
+        This project was coded by Mona Lang and is{" "}
+        <a
+          href="https://github.com/mlang1988/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
       </footer>
     </div>
   );
