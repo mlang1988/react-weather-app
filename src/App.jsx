848389aba6616cb36c85a7c269a248ead73a5aa8
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-03-18 10:03:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Mona Lang and is open-sourced on GitHub.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
