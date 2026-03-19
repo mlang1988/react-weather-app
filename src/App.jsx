f74a076461fc7d87337501c8ef987c2d81794183
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-03-19 11:53:12 +0100

    Added api integration

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Berlin" />
         <footer>
           This project was coded by Mona Lang and is{" "}
           <a
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,45 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temp: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: "Wednesday 17:00",
+      description: response.data.condition.description,
+      iconUrl:
+        "https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg",
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <div className="d-flex justify-content-between">
+          <div>
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>
+                {weatherData.date}, {weatherData.description}
+              </li>
+              <li>
+                Humidity: {weatherData.humidity}%, Wind: {weatherData.wind}/h
+              </li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <div className="d-flex justify-content-between">
-        <div>
-          <h1>Berlin</h1>
-          <ul>
-            <li>Wednesday 17:00, Sunny</li>
-            <li>Humidity: 46%, Wind: 10km/h</li>
-          </ul>
-        </div>
-        <div>
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/sunny_light.svg"
-            alt="Sunny"
-          />
-          <span className="temp">6</span>
-          <span className="unit">°C</span>
+          <div>
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
+            <span className="temp">{Math.round(weatherData.temp)}</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b0c48dt2da1edfa05b13oc7376330d93";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
