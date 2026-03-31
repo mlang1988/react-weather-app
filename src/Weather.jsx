a76770139f75c106782cb60e9682f78ddf763e8e
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-03-31 16:27:16 +0200

    Optimized code

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -15,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      iconUrl: `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       wind: response.data.wind.speed,
       city: response.data.city,
     });
