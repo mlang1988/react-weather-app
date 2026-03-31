94bc6b25ec41db568ccbc176e480c09e7ab0d268
mlang1988 <165694522+mlang1988@users.noreply.github.com>
2026-03-31 15:16:26 +0200

    Added loop for 5 day forecast

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -16,9 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <ForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
